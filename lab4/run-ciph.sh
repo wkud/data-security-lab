78d869c56d946cc61028c872c72d80871b1cb464
Wojciech <vopox07@gmail.com>
2021-12-21 17:51:35 +0100

    lab4-done

---
+++ lab4/run-ciph.sh
@@ -0,0 +1,7 @@
+# to compile and run CIPH.CPP, use this commands:
+g++ -o ciph.exe CIPH.cpp
+./ciph.exe
+
+# to compile and run DECIPH.CPP, use this commands:
+g++ -o deciph.exe DECIPH.cpp
+./deciph.exe
